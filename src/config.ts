dda173a1c15b95c1114363402540e83e9881a8df
malikfaizan111 <faizanatifmasood@gmail.com>
2023-07-14 06:56:45 +0500

    initialize branch

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+export const appConfig =
+{
+    // local
+    // base_url: 'http://127.0.0.1:2500/api/v1/afforestation/',
+    // file_url: 'http://127.0.0.1:2500/api/v1/afforestation/',
+
+    // staging
+    base_url: 'http://13.126.174.129:2500/api/v1/afforestation/',
+    file_url: 'http://13.126.174.129:2500/api/v1/afforestation/',
+
+    // Live
+    // base_url: 'https://api.greenertomorrow.pk/api/v1/afforestation/',
+    // file_url: 'https://api.greenertomorrow.pk/api/v1/afforestation/',
+    base_url_slug: '', // admin | gardener
+
+    default_auth_key: 'andpercent',
+
+    google_api_key: 'AIzaSyDykNkzGorhQSSUDKkJUzSYFMK04_YeHQ4',
+}
+
+
